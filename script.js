67614b14442b2c00fcb4c071fcf4ee4ee021997e
Diana <kabadiana327@gmail.com>
2022-10-23 13:06:05 +0300

    regardless

--- script.js
+++ script.js
@@ -16,8 +16,12 @@ function trafficLight(name) {
 
 let trafficLight1 = trafficLight(1);
 console.log(trafficLight1());
+console.log(trafficLight1());
 let trafficLight2 = trafficLight(2);
+console.log(trafficLight2());
+console.log(trafficLight1());
 console.log(trafficLight2());
 let trafficLight3 = trafficLight(3);
 console.log(trafficLight3());
 console.log(trafficLight2());
+console.log(trafficLight1());
