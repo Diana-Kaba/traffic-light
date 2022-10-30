7d591f391e098a9cdd43f3738c85a02d80365126
Diana <kabadiana327@gmail.com>
2022-10-30 12:23:09 +0200

    color

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ function trafficLight(name) {
     } else {
       color = "red";
     }
-    return name + ":" + color;
+    let arr = {};
+    arr.name = name;
+    arr.color = color;
+    return arr;
   };
   return examination;
 }
 
 let trafficLight1 = trafficLight(1);
 console.log(trafficLight1());
+let a = trafficLight1();
+console.log(`%c ${a.name}: ${a.color}`, `background:${a.color}`);
 console.log(trafficLight1());
 let trafficLight2 = trafficLight(2);
 console.log(trafficLight2());
